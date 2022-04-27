19ebd6db2dbe631f893adbe438dc436d90dea62e
Ethan Steller <steller,e@gmail.com>
2022-04-27 18:57:01 -0500

    Database setup

--- src/main/java/Personal/StorageApp/controllers/HomeController.java
+++ src/main/java/Personal/StorageApp/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package Personal.StorageApp.controllers;
 
+import Personal.StorageApp.models.data.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private ItemRepository itemRepository;
 
     @RequestMapping(value = "/")
     public String index(){
