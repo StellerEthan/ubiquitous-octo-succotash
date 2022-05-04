aed21e716d29c3f4e43e21872bf270deedcda542
Ethan Steller <steller,e@gmail.com>
2022-05-03 21:02:21 -0500

    set up to accept POST request from front end.

--- src/main/java/Personal/StorageApp/controllers/ItemController.java
+++ src/main/java/Personal/StorageApp/controllers/ItemController.java
@@ -3,23 +3,24 @@ package Personal.StorageApp.controllers;
 import Personal.StorageApp.models.Item;
 import Personal.StorageApp.models.data.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/items")
-public class HomeController {
+public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
 
     @GetMapping
-    @CrossOrigin(origins = "http://localhost:3000")
     public List<Item> getAllItems(){
         return itemRepository.findAll();
     }
 
+    @PostMapping
+    public Item createItem(@RequestBody Item item){
+        return itemRepository.save(item);
+    }
 }
